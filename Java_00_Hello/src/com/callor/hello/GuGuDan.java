c45273eac5af9377936e58c2ca89ed0ec1c31321
minuk0506 <leeminuk9238@daum.net>
2022-03-24 10:56:14 +0900

    src

---
+++ Java_00_Hello/src/com/callor/hello/GuGuDan.java
@@ -0,0 +1,19 @@
+package com.callor.hello;
+
+public class GuGuDan {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("구구단 3단");
+		System.out.println("================================");
+		System.out.println("3x2="+3*2);
+		System.out.println("3x3="+3*3);
+		System.out.println("3x4="+3*4);
+		System.out.println("3x5="+3*5);
+		System.out.println("3x6="+3*6);
+		System.out.println("3x7="+3*7);
+		System.out.println("3x8="+3*8);
+		System.out.println("3x9="+3*9);
+		System.out.println("================================");
+	}
+}
