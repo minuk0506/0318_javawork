42ba76be4f10179fcc6f3e5095f07a63a78eaf0d
minuk0506 <leeminuk9238@daum.net>
2022-04-19 22:34:27 +0900

    0419수정V2

--- Java_40_Classes_03/src/com/callor/score/service/ScoreServiceV1.java
+++ Java_40_Classes_03/src/com/callor/score/service/ScoreServiceV1.java
@@ -6,76 +6,77 @@ import com.callor.score.utils.Line;
 public class ScoreServiceV1 {
 
 	private ScoreV2VO[] scores;
-	private int sumKor;
-	private int sumEng;
-	private int sumMath;
-	private int sumSum;
-	private float sumfAvg;
-	private int intStudent;
-	
+
 	public ScoreServiceV1() {
 		this(10);
 	}
-	
+
 	public ScoreServiceV1(int length) {
-		
+
 		scores = new ScoreV2VO[length];
-		for(int i = 0 ; i < scores.length ; i++) {
+		for (int i = 0; i < scores.length; i++) {
 			scores[i] = new ScoreV2VO();
 		}
-		
+
 	}
+
 	// 학생 이름 리스트를 배열로 받아서 초기화 하기
 	public ScoreServiceV1(String[] stNames) {
-		
+
 		int length = stNames.length;
 		scores = new ScoreV2VO[length];
-		for(int i = 0 ; i < length ; i++) {
+		for (int i = 0; i < length; i++) {
 			scores[i] = new ScoreV2VO();
 			scores[i].setStName(stNames[i]);
 		}
 	}
-	
+
 	public void makeScore() {
-		
-		
-		for(int i = 0; i < scores.length; i++) {
-			int intKor = (int)(Math.random() * 50) + 51;
-			int intEng = (int)(Math.random() * 50) + 51;
-			int intMath = (int)(Math.random() * 50) + 51;
-			
+
+		for (int i = 0; i < scores.length; i++) {
+			int intKor = (int) (Math.random() * 50) + 51;
+			int intEng = (int) (Math.random() * 50) + 51;
+			int intMath = (int) (Math.random() * 50) + 51;
+
 			scores[i].setIntKor(intKor);
 			scores[i].setIntEng(intEng);
 			scores[i].setIntMath(intMath);
-			
-			sumKor += scores[i].getIntKor();
-			sumEng += scores[i].getIntEng();
-			sumMath += scores[i].getIntMath();
-			sumSum += scores[i].getIntSum();
-			sumfAvg += scores[i].getfAvg();
-			intStudent = i+1;
-		}	
-		sumfAvg /= intStudent;
+
+		}
+
 	}
+
 	public void printScore() {
+		int sumKor = 0;
+		int sumEng = 0;
+		int sumMath = 0;
+		int sumSum = 0;
+		float sumfAvg = 0;
+		int intStudent = 0;
 		System.out.println(Line.dLine(50));
 		System.out.println("빛나고교 성적표 v1");
 		System.out.println(Line.sLine(50));
 		System.out.println("이  름\t국어\t영어\t수학\t총점\t평균");
 		System.out.println(Line.sLine(50));
-		
-		for(int i = 0 ; i <scores.length ; i++) {
+
+		for (int i = 0; i < scores.length; i++) {
+			sumKor += scores[i].getIntKor();
+			sumEng += scores[i].getIntEng();
+			sumMath += scores[i].getIntMath();
+			sumSum += scores[i].getIntSum();
+			sumfAvg += scores[i].getfAvg();
+			intStudent = i + 1;
 			System.out.print(scores[i].getStName() + "\t");
 			System.out.print(scores[i].getIntKor() + "\t");
 			System.out.print(scores[i].getIntEng() + "\t");
 			System.out.print(scores[i].getIntMath() + "\t");
 			System.out.print(scores[i].getIntSum() + "\t");
 			System.out.printf("%3.2f\n", scores[i].getfAvg());
-			
+
 		}
+		sumfAvg /= intStudent;
 		System.out.println(Line.sLine(50));
 		System.out.printf("  계\t%d\t%d\t%d\t%d\t%3.2f", sumKor, sumEng, sumMath, sumSum, sumfAvg);
-		
-		
+
 	}
 }
