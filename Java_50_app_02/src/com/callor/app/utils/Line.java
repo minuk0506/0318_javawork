07ba1ef799379f1371aa5f93ff6bd0e242b98ab5
minuk0506 <leeminuk9238@daum.net>
2022-04-26 16:43:25 +0900

    0426java

---
+++ Java_50_app_02/src/com/callor/app/utils/Line.java
@@ -0,0 +1,11 @@
+package com.callor.app.utils;
+
+public class Line {
+
+	public static String dLine(int length) {
+		return "=".repeat(length);
+	}
+	public static String sLine(int length) {
+		return "-".repeat(length);
+	}
+}
