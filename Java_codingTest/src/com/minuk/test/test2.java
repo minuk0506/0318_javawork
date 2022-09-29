a8b2ee208f3142e03e1e909df75afc551ed53fbc
minuk0506 <leeminuk9238@daum.net>
2022-09-29 14:40:27 +0900

    0929

--- Java_codingTest/src/com/minuk/test/test2.java
+++ Java_codingTest/src/com/minuk/test/test2.java
@@ -4,11 +4,13 @@ public class test2 {
 	public static void main(String[] args) {
 		long n = 12345;
 		String nStr = String.valueOf(n);
-		System.out.println(nStr);
+		StringBuilder sb = new StringBuilder(nStr);
+		sb = sb.reverse();
+		System.out.println(sb);
 		int[] answer = {};
 		for(int i = 0; i <= n; i++) {
-			answer[i] = (int)nStr.substring();
-			System.out.println("nStr값"+nStr.split(""));
+			//answer[i] = (int)nStr.substring();
+			//System.out.println("nStr값"+nStr.split(""));
 		}
 	}
 }
